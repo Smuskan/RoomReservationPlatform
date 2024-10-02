1271c804e277cad145116a883a13c073c6b9b4f5
Muskan <muskanshaikh172003@gmail.com>
2024-10-02 21:00:01 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Hotel Booking</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   
+    
+    <style>
+        .well
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+    
+        }
+        .wellfix
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+            height: 150px;
+        }
+		body
+		{
+			background-image: url('images/home_bg.jpg');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
+		p
+		{
+			font-size: 13px;
+		}
+        .pro_pic
+        {
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            margin-bottom: 15px;
+            margin-right: 15px;
+        }
+		
+    </style>
+    
+    
+</head>
+
+<body>
+    <div class="container">
+      
+       <img class="img-responsive" src="images/gpa.png" style="width:100%; height:180px;">
+       <img class="img-responsive" src="images/home_banner.jpg" style="width:100%; height:180px;">      
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="room.php">Room &amp; Facilities</a></li>
+                    <li><a href="reservation.php">Online Reservation</a></li>
+                    <li><a href="admin.php">Admin</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://www.facebook.com"><img src="images/facebook.png"></a></li>
+                    <li><a href="http://www.twitter.com"><img src="images/twitter.png"></a></li>
+                    
+                </ul>
+            </div>
+        </nav>
+
+     
+        <div class="jumbotron">
+        <div class="w3-content w3-section">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/1.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/2.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/3.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/4.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/5.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/6.jpg" style="width:100%; height:450px;">
+        </div>    
+        </div>
+        <hr>
+        <div class="row" style="color: #ed9e21">
+            <div class="col-md-12 well" >
+              <h4><strong style="color: #ffbb2b">About</strong></h4><br>
+              <p>WELCOME TO GPA
+HOTEL IN AWASARI:-
+Here at GPA, we embrace our guests with comfort and value, enhancing each stay through the uniqueness of our city and team. Perfect for business and leisure travelers, our boutique hotel is a comfortable escape in the heart of downtown San Francisco.</p>
+              <br>
+              <p>MEET ME IN AWASARI:-
+Weddings, family reunions, executive meetings, holiday parties—we welcome every celebration and meeting here at GPA Hotel.
+
+Our hotel offers more than 5,000 square feet of event space throughout three indoor and outdoor venues. Our largest space is the Rooftop Terrace, which is located on the eighth floor and offers beautiful views of the San Francisco skyline.
+
+
+</p>
+              <br>
+
+              
+            </div>  
+        </div>
+        <div class="row" style="color: #ffbb2b">
+            <div class="col-md-4 wellfix">
+              <h4><strong>Contact Us</strong></h4><hr>
+              Address :  Awasari (Kh.) Ambegoan , District : Pune<br>
+              Mail :gpawasari@gmail.com<br>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 wellfix">
+                <h4><strong>Developed By</strong></h4><hr>
+                <a href="#">Nalawade Siddhi</a>
+				<br><a href="#">Sanap Vrushali</a>
+				<br><a href="#">Udavant Payal</a>
+                <br><a href="#">Ghadaghe Omkar</a>
+
+            </div>
+        </div>
+        
+
+
+
+    </div>
+    
+    
+    
+    
+    
+
+
+
+
+    <script src="my_js/slide.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
